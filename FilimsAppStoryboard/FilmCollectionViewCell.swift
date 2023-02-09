498eb30a3f8de0585a4571a8469ae8572c758650
Ruslan <hellperock@gmail.com>
2023-02-09 15:59:25 +0500

    first commit

---
+++ FilimsAppStoryboard/FilmCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FilmCollectionViewCell.swift
+//  FilimsAppStoryboard
+//
+//  Created by Ruslan Ismailov on 08/02/23.
+//
+
+import UIKit
+
+class FilmCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FilimsAppStoryboard/JSONModel.swift
@@ -0,0 +1,8 @@
+//
+//  JSONModel.swift
+//  FilimsAppStoryboard
+//
+//  Created by Ruslan Ismailov on 07/02/23.
+//
+
+import Foundation
